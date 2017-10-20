189f84ce95f8818c1ca0c43b1af99d7851bd7d8f
Tomás Riquelme <triquelme@colegium.com>
2017-10-20 11:05:07 -0300

    avances desde el pc de colegium

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,17 +14,20 @@ export class AppComponent implements OnInit {
     public identity;
     public token;
     public errorMessage;
+    public user_register: User;
+    public alertRegister;
 
     constructor(private _userService: UserService) {
         this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+        this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
     }
 
     ngOnInit() {
         this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
 
-        console.log(this.identity);
-        console.log(this.token);
+        //console.log(this.identity);
+        //console.log(this.token);
     }
 
     public enSubmit() {
@@ -80,11 +83,37 @@ export class AppComponent implements OnInit {
         );
     }
 
-    logout (){
+    logout() {
         localStorage.removeItem("token");
         localStorage.removeItem("identity");
         localStorage.clear();
         this.identity = null;
         this.token = null;
+    }
+
+    enSubmitRegister() {
+        console.log(this.user_register);
+        this._userService.register(this.user_register).subscribe(
+            response => {
+                let user = response.user;
+                this.user_register = user;
+
+                if(!user._id){
+                    alert('Error al registrarse');
+                }else{
+                    this.alertRegister = 'El registro se ha realizado correctamente, identificate con '+this.user_register.email;
+                    this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
+                }
+            },
+            error => {
+                var errorMessage = <any> error;
+
+                if (error != null) {
+                    var body = JSON.parse(error._body);
+                    this.alertRegister = body.message;
+                    console.log(error);
+                }
+            }
+        );
     }
 }
